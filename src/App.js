09d80a0dfb23bc52dcc23d3a7374505bdc162eb8
Andrew Heekin <andrewheekin@gmail.com>
2017-10-15 18:23:31 -0400

    clear the session on logout

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import { Nav, NavItem, Navbar } from 'react-bootstrap';
 import Routes from './Routes';
 import RouteNavItem from './components/RouteNavItem';
-import { authUser } from './libs/awsLib';
+import { authUser, signOutUser } from './libs/awsLib';
 import './App.css';
 
 class App extends Component {
@@ -21,55 +21,59 @@ class App extends Component {
       if (await authUser()) {
         this.userHasAuthenticated(true);
       }
-    }
-    catch(e) {
+    } catch (e) {
       alert(e);
     }
-  
+
     this.setState({ isAuthenticating: false });
-  }  
+  }
 
   userHasAuthenticated = authenticated => {
     this.setState({ isAuthenticated: authenticated });
   };
 
   handleLogout = event => {
+    signOutUser();
+  
     this.userHasAuthenticated(false);
-  };
+  }
 
   render() {
     const childProps = {
       isAuthenticated: this.state.isAuthenticated,
-      userHasAuthenticated: this.userHasAuthenticated
+      userHasAuthenticated: this.userHasAuthenticated,
     };
-  
+
     return (
-      !this.state.isAuthenticating &&
-      <div className="App container">
-        <Navbar fluid collapseOnSelect>
-          <Navbar.Header>
-            <Navbar.Brand>
-              <Link to="/">Scratch</Link>
-            </Navbar.Brand>
-            <Navbar.Toggle />
-          </Navbar.Header>
-          <Navbar.Collapse>
-            <Nav pullRight>
-              {this.state.isAuthenticated
-                ? <NavItem onClick={this.handleLogout}>Logout</NavItem>
-                : [
+      !this.state.isAuthenticating && (
+        <div className="App container">
+          <Navbar fluid collapseOnSelect>
+            <Navbar.Header>
+              <Navbar.Brand>
+                <Link to="/">Scratch</Link>
+              </Navbar.Brand>
+              <Navbar.Toggle />
+            </Navbar.Header>
+            <Navbar.Collapse>
+              <Nav pullRight>
+                {this.state.isAuthenticated ? (
+                  <NavItem onClick={this.handleLogout}>Logout</NavItem>
+                ) : (
+                  [
                     <RouteNavItem key={1} href="/signup">
                       Signup
                     </RouteNavItem>,
                     <RouteNavItem key={2} href="/login">
                       Login
-                    </RouteNavItem>
-                  ]}
-            </Nav>
-          </Navbar.Collapse>
-        </Navbar>
-        <Routes childProps={childProps} />
-      </div>
+                    </RouteNavItem>,
+                  ]
+                )}
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
+          <Routes childProps={childProps} />
+        </div>
+      )
     );
   }
 }
